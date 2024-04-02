7f492310c9538fd1a781004ea24611c4b1dc10e7
SabaliDev <127083776+SabaliDev@users.noreply.github.com>
2024-04-03 02:10:32 +0300

    leaning-nest

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Request } from "express";
+import { CreateCatDto } from "./dto/create-cat.dto";
+
+
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  // @Get()
+  // findAll(@Query() query: ListAllEntities) {
+  //   return `This action returns all cats (limit: ${query.limit} items)`;
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
